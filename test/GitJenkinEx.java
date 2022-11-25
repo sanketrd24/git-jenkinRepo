6c453bd1ffc45a16ca8ff159d0a1f8b342f41d34
Sanket Dongre <sanket.dongre@morningstar.com>
2022-11-25 10:59:30 +0530

    second commit

---
+++ test/GitJenkinEx.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.Iterator;
+
+public class GitJenkinEx {
+public static void main(String[] args) {
+	for (int i = 0; i < 5; i++) {
+		System.out.println(i);
+	}
+}
+}
